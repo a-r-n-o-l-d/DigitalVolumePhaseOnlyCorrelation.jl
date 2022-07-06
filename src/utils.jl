662fb1bc02eb090e734ed36a515bcbaf78a7eea7
a-r-n-o-l-d <arnold.fertin@univ-grenoble-alpes.fr>
2022-07-06 15:04:15 +0200

    utils.jl padsignals

--- src/utils.jl
+++ src/utils.jl
@@ -13,3 +13,15 @@ function refinedisp(img1, img2, I1, Δ, apod, subpix)
     Δest, cor = localdisp(img1, img2, I1, I2, apod, subpix)
     (Δi .+ Δest), cor
 end
+
+function padsignals(sig1, sig2, win1, win2, Δinit, pad)
+    spad = @. (win1 + win2) ÷ 2 + abs(round(Int, Δinit))
+    if pad == :fill0
+        im1 = padarray(img1, Fill(zero(eltype(img1)), spad))
+        im2 = padarray(img2, Fill(zero(eltype(img2)), spad))
+    else
+        im1 = padarray(img1, Pad(pad, sz))
+        im2 = padarray(img2, Pad(pad, sz))
+    end
+    im1, im2
+end
